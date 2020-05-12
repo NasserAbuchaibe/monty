e01b03b5ad7b93076a339c73dcc8339e07221e66
Camilo tobon <1611@holbertonschool.com>
2020-05-12 16:19:37 -0500

    add

--- main.c
+++ main.c
@@ -4,11 +4,10 @@ int main()
 {
 	stack_t *stack;
 
-	push(&stack, 100);
-	push(&stack, 50);
-	push(&stack, 10);
-	push(&stack, 500);
-	push(&stack, 30);
+	push(&stack, 1);
+	push(&stack, 2);
+	push(&stack, 3);
+
 
 	printall(&stack);
 }
