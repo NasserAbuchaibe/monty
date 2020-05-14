35e07dd437529002a809a00e8ebed0fcd2f56464
Camilo tobon <1611@holbertonschool.com>
2020-05-14 12:49:52 -0500

    fixing div

--- test/main.c
+++ test/main.c
@@ -7,8 +7,8 @@ int main(int argc, char **argv)
 	stack_t *stack = malloc(sizeof(stack_t));
 	
 	push(&stack, 3);
-	push(&stack, 0);
 	push(&stack, 5);
+	push(&stack, 30);
 	_div(&stack, 3);
 	pall(&stack);
 }
--- test/stackAuxiliars.c
+++ test/stackAuxiliars.c
@@ -1,27 +1,24 @@
-/*
- * Aux_Functions.c
- *
- *  Created on: 12/05/2020
- *      Author: deepzirox
- */
-
 #include "monty.h"
-
-void _div(stack_t **head, int line_number)
+/**
+ * _div - div second top by top
+ * @stack: head of linked list
+ * @line_number - number of line of the file readed
+ */
+void _div(stack_t **stack, unsigned int line_number)
 {
-	stack_t *aux = *head;
-	float res;
+	stack_t *aux = *stack;
+	int res;
 
 	if (n_nodes(aux) < 2)
 	{
-		fprintf(stderr,"<%d>: can't swap, stack too short\n", line_number);
+		fprintf(stderr,"L%d: can't div, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	while (aux->next != NULL)
 		aux = aux->next;
 
-	res = aux->n /= aux->prev->n;
+	res = (float)aux->prev->n / (float)aux->n;
 	res = (int)res;
 	aux->prev->n = res;
 	aux->prev->next = NULL;
