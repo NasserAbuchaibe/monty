f65aa2bf8507493dd2f1db7ba9027bf96182fd70
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2020-05-14 17:23:06 +0000

    fix fail

--- stackAuxiliars.c
+++ stackAuxiliars.c
@@ -8,7 +8,7 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *aux = *stack;
 
-	if (n_nodes(aux) < 2)
+	if (n_nodes(aux) < 2 || stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		ll_free(stack);
